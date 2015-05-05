78f035347e94cad5812ec81291e9e10a63c2ad9b
Maxwell Berkel <maxwell@allbitsnbytes.com>
2015-05-05 16:54:45 -0400

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 // Dependencies
-var $			= require('gulp-load-plugins');
+var $			= require('gulp-load-plugins')();
 var gulp		= require('gulp');
+var jeet		= require('jeet');
 
 
 // Paths
@@ -21,13 +22,28 @@ var paths = {
 gulp.task('default', ['css', 'js']);
 
 
+// Watch 
+gulp.task('watch', function() {
+	gulp.watch(paths.src.css, 'css');
+	gulp.watch(paths.src.js, 'js');
+});
+
+
 // Process all stylus files
 gulp.task('css', function() {
-	
+	return gulp.src(paths.src.css)
+		.pipe($.stylus({
+			use: [jeet]
+		}))
+		.pipe($.minifyCss())
+		.pipe($.autoprefixer())
+		.pipe(gulp.dest(paths.build.css));
 });
 
 
 // Process all javascript files
 gulp.task('js', function() {
-	
+	return gulp.src(paths.src.js)
+		.pipe($.uglify())
+		.pipe(gulp.dest(paths.build.js));
 });
