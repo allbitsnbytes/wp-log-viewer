526d52263e577c5294537377f52d7bd5bc1a98fe
Maxwell Berkel <maxwell@allbitsnbytes.com>
2015-05-05 19:17:36 -0400

    Updated files

--- libs/Plugin.php
+++ libs/Plugin.php
@@ -45,50 +45,56 @@ class Plugin {
 	 * Add navigation entry
 	 */
 	public function add_navigation() {
-		add_management_page('Wordpress Log Viewer', 'Log Viewer', 'manage_options', 'wp-log-viewer', function() {
-			echo '
-				<div id="wp-log-viewer" class="wrap">
-					<h2>Log Viewer</h2>
-					
-					<div class="viewer">
-					</div>
-					
-					<div class="sidebar">
-						<h3>Actions</h3>
-						<ul>
-							<li><a href="#" title="Clear debug.log">Clear</a></li>
-							<li><a href="#" tilte="Download file">Download</a></li>
-						</ul>
+		add_management_page('Wordpress Log Viewer', 'Log Viewer', 'manage_options', 'wp-log-viewer', [$this, 'get_view']);
+	}
 
-						<h3>Date Range</h3>
-						<ul>
-							<li><a href="#" title="Show today">Today</a></li>
-							<li><a href="#" tilte="Show yesterday">Yesterday</a></li>
-							<li><a href="#" title="This Week">This week</a></li>
-							<li><a href="#" title="This Month">This month</a></li>
-						</ul>
-						
-						<h3>Sort</h3>
-						<ul>
-							<li><a href="#" title="Sort by most recent">Most recent</a></li>
-							<li><a href="#" tilte="Sort by oldest">Oldest first</a></li>
-						</ul>
-						
-						<h3>Severity</h3>
-						<ul>
-							<li><a href="#" title="All errors">All</a></li>
-							<li><a href="#" title="Fatal errors">Fatal</a></li>
-							<li><a href="#" title="Warnings errors">Warnings</a></li>
-							<li><a href="#" title="Misc errors">Misc</a></li>
-						</ul>
-						
-						<small>
-							<strong>Last modified</strong><br>
-							<span class="last-modified">today</span>
-						</small>
-					</div>
+
+	/**
+	 * Get main view
+	 */
+	public function get_view() {
+		echo '
+			<div id="wp-log-viewer" class="wrap">
+				<h2>Log Viewer</h2>
+				
+				<div class="viewer">
 				</div>
-			';
-		});
+				
+				<div class="sidebar">
+					<h3>Actions</h3>
+					<ul>
+						<li><a href="#" title="Clear debug.log">Clear</a></li>
+						<li><a href="#" tilte="Download file">Download</a></li>
+					</ul>
+
+					<h3>Date Range</h3>
+					<ul>
+						<li><a href="#" title="Show today">Today</a></li>
+						<li><a href="#" tilte="Show yesterday">Yesterday</a></li>
+						<li><a href="#" title="This Week">This week</a></li>
+						<li><a href="#" title="This Month">This month</a></li>
+					</ul>
+					
+					<h3>Sort</h3>
+					<ul>
+						<li><a href="#" title="Sort by most recent">Most recent</a></li>
+						<li><a href="#" tilte="Sort by oldest">Oldest first</a></li>
+					</ul>
+					
+					<h3>Severity</h3>
+					<ul>
+						<li><a href="#" title="All errors">All</a></li>
+						<li><a href="#" title="Fatal errors">Fatal</a></li>
+						<li><a href="#" title="Warnings errors">Warnings</a></li>
+						<li><a href="#" title="Misc errors">Misc</a></li>
+					</ul>
+					
+					<small>
+						<strong>Last modified</strong><br>
+						<span class="last-modified">today</span>
+					</small>
+				</div>
+			</div>
+		';
 	}
 }
