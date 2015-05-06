e237346c16811ca19f753e95618b447df6596109
Maxwell Berkel <maxwell@allbitsnbytes.com>
2015-05-06 15:02:09 -0400

    Updated Plugin.php

--- libs/Plugin.php
+++ libs/Plugin.php
@@ -34,10 +34,10 @@ class Plugin {
 	 */
 	public function load_css_and_js() {
 		// Stylesheet files
-		wp_enqueue_style('wplogviewer-css', WPLOGVIEWER_URL . 'assets/css/main.css');
-		
+		wp_enqueue_style('wplogviewer-css', WPLOGVIEWER_URL . 'assets/css/main.min.css');
+
 		// Javascript files
-		wp_enqueue_script('wplogviewer-js', WPLOGVIEWER_URL . 'assets/js/main.js');
+		wp_enqueue_script('wplogviewer-js', WPLOGVIEWER_URL . 'assets/js/main.min.js', false, false, true);
 	}
 	
 	
@@ -57,43 +57,7 @@ class Plugin {
 			<div id="wp-log-viewer" class="wrap">
 				<h2>Log Viewer</h2>
 				
-				<div class="viewer">
-				</div>
-				
-				<div class="sidebar">
-					<h3>Actions</h3>
-					<ul>
-						<li><a href="#" title="Clear debug.log">Clear</a></li>
-						<li><a href="#" tilte="Download file">Download</a></li>
-					</ul>
-
-					<h3>Date Range</h3>
-					<ul>
-						<li><a href="#" title="Show today">Today</a></li>
-						<li><a href="#" tilte="Show yesterday">Yesterday</a></li>
-						<li><a href="#" title="This Week">This week</a></li>
-						<li><a href="#" title="This Month">This month</a></li>
-					</ul>
-					
-					<h3>Sort</h3>
-					<ul>
-						<li><a href="#" title="Sort by most recent">Most recent</a></li>
-						<li><a href="#" tilte="Sort by oldest">Oldest first</a></li>
-					</ul>
-					
-					<h3>Severity</h3>
-					<ul>
-						<li><a href="#" title="All errors">All</a></li>
-						<li><a href="#" title="Fatal errors">Fatal</a></li>
-						<li><a href="#" title="Warnings errors">Warnings</a></li>
-						<li><a href="#" title="Misc errors">Misc</a></li>
-					</ul>
-					
-					<small>
-						<strong>Last modified</strong><br>
-						<span class="last-modified">today</span>
-					</small>
-				</div>
+				<div id="viewer-pane"></div>
 			</div>
 		';
 	}
