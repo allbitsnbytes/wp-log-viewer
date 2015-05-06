b71673fc0263113ba93ee7341a5cc39393330f39
Maxwell Berkel <maxwell@allbitsnbytes.com>
2015-05-06 19:45:42 -0400

    Add Log.php to access and manipulate debug.log

---
+++ libs/Log.php
@@ -0,0 +1,87 @@
+<?php 
+	
+namespace Allbitsnbytes\WPLogViewer;
+
+if (!defined('WPLOGVIEWER_BASE')) {
+	header('HTTP/1.0 403 Forbidden');
+	die;
+}
+
+
+/**
+ * Dependencies
+ */
+use Allbitsnbytes\WPLogViewer\Characteristic\IsSingleton;
+use Allbitsnbytes\WPLogViewer\Helper;
+
+
+/**
+ * Log file handler
+ * @since 0.1.0
+ */
+class Log {
+	
+	use IsSingleton;
+	
+	/**
+	 * Check if debug.log file exists
+	 * @return boolean True if debug.log exists
+	 * @since 0.1.0
+	 */
+	public function file_exists() {
+		// TODO
+	}
+	
+	
+	/**
+	 * Check if debugging is enabled
+	 * @return boolean True if debugging is enabled
+	 * @since 0.1.0
+	 */
+	public function debug_enabled() {
+		// TODO
+	}
+	
+	
+	/**
+	 * Check if file was modified since provided timestamp
+	 * @param $timestamp int Timestamp to check against
+	 * @return boolean True if was modified
+	 * @since 0.1.0
+	 */
+	public function is_modified($timestamp) {
+		// TODO
+	}
+	
+	
+	/**
+	 * Check if file is smaller in size than provided size in bytes
+	 * @param $size int Size to check against
+	 * @return boolean True if file is smaller
+	 * @since 0.1.0
+	 */
+	public function is_smaller($size) {
+		// TODO
+	}
+	
+	
+	/**
+	 * Get all log entries
+	 * @return array Array of entries
+	 * @since 0.1.0
+	 */
+	public function get_entries() {
+		// TODO
+	}
+	
+	
+	/**
+	 * Get all entries newer than specified timestamp
+	 * @param $timestamp int The oldest date and time to get entries for
+	 * @return array Array of entries
+	 * @since 0.1.0
+	 */
+	public function get_recent_entries($timestamp) {
+		// TODO
+	}
+}
