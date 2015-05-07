1303e86006b1183b413d40bdc07550af2bb34973
Maxwell Berkel <maxwell@allbitsnbytes.com>
2015-05-07 14:36:36 -0400

    Added Api.php to process api requests

---
+++ libs/Api.php
@@ -0,0 +1,188 @@
+<?php
+	
+namespace Allbitsnbytes\WPLogViewer;
+
+if (!defined('WPLOGVIEWER_BASE')) {
+	header('HTTP/1.0 403 Forbidden');
+	die;
+}
+
+
+/**
+ * Dependencies
+ */
+use Allbitsnbytes\WPLogViewer\Helper;
+use Allbitsnbytes\WPLogViewer\Log;
+
+
+/**
+ * Handle authentication
+ * @since 0.1.0
+ */
+class Api {
+	
+	/**
+	 * Get log file details
+	 * @param Request $req Request instance
+	 * @param Response $res Response instance
+	 * @return Response The current response instance
+	 * @since 0.1.0
+	 */
+	public static function get_log_details($req, $res) {
+		$log = Log::getInstance();
+	
+		$res->setJSON([
+			'entries'		=> $log->get_entries(),
+			'found'			=> $log->file_exists(),
+			'debug_enabled'	=> $log->debug_enabled(),
+			'timezone'		=> $log->get_timezone(),
+			'modified'		=> $log->last_modified(),
+		]);
+	
+		return $res;
+	}
+	
+
+	/**
+	 * Check if log file has been modified
+	 * @param Request $req Request instance
+	 * @param Response $res Response instance
+	 * @return Response The current response instance
+	 * @since 0.1.0
+	 */
+	public static function check_if_log_modified($req, $res) {
+		$log = Log::getInstance();
+		
+		$res->setJSON([
+			'modified'		=> $log->is_modified(),
+			'truncated'		=> $log->is_smaller(),
+		]);
+		
+		return $res;
+	}
+	
+
+	/**
+	 * Check if log file exists
+	 * @param Request $req Request instance
+	 * @param Response $res Response instance
+	 * @return Response The current response instance
+	 * @since 0.1.0
+	 */
+	public static function check_if_log_exists($req, $res) {
+		$log = Log::getInstance();
+		
+		$res->setJSON([
+			'exists'		=> $log->file_exists(),
+		]);
+		
+		return $res;
+	}
+	
+
+	/**
+	 * Check if debugging is enabled
+	 * @param Request $req Request instance
+	 * @param Response $res Response instance
+	 * @return Response The current response instance
+	 * @since 0.1.0
+	 */
+	public static function check_if_debug_enabled($req, $res) {
+		$log = Log::getInstance();
+		
+		$res->setJSON([
+			'debug_enabled'	=> $log->debug_enabled(),
+		]);
+		
+		return $res;
+	}
+	
+
+	/**
+	 * Get log entries
+	 * @param Request $req Request instance
+	 * @param Response $res Response instance
+	 * @return Response The current response instance
+	 * @since 0.1.0
+	 */
+	public static function get_log_entries($req, $res) {
+		$log = Log::getInstance();
+		$truncated = $log->is_smaller();
+
+		$res->setJSON([
+			'truncated'		=> $truncated,
+			'entries'		=> $truncated ? $log->get_recent_entries() : $log->get_entries(),
+			'modified'		=> $log->last_modified(),
+		]);
+		
+		return $res;
+	}
+	
+	
+	/**
+	 * Get log entries if log file has been modified
+	 * @param Request $req Request instance
+	 * @param Response $res Response instance
+	 * @return Response The current response instance
+	 * @since 0.1.0
+	 */
+	public static function get_log_entries_if_modified($req, $res) {
+		$log = Log::getInstance();
+		
+		if ($log->is_modified()) {
+			return self::get_log_entries($req, $res);
+		} 
+		
+		$res->setJSON([
+			'modified'		=> false,
+		]);
+		
+		return $res;
+	}
+	
+	
+	/**
+	 * Clear log file
+	 * @param Request $req Request instance
+	 * @param Response $res Response instance
+	 * @return Response The current response instance
+	 * @since 0.1.0
+	 */
+	public static function clear_log($req, $res) {
+		$log = Log::getInstance();
+		
+		$res->setJSON([
+			'cleared'		=> $log->clear(),
+		]);
+		
+		return $res;
+	}
+	
+	
+	/**
+	 * Process user login
+	 * @param Request $req Request instance
+	 * @param Response $res Response instance
+	 * @return Response The current response instance
+	 * @since 0.1.0
+	 *
+	 * @todo
+	 *		- Add brute force protection
+	 *		- Limit login attempts
+	 */
+	public static function login_user($req, $res) {
+		// TODO
+	}
+	
+	
+	/**
+	 * Process user login
+	 * @param Request $req Request instance
+	 * @param Response $res Response instance
+	 * @return Response The current response instance
+	 * @since 0.1.0
+	 */
+	public static function logout_user($req, $res) {
+		// TODO
+	}
+}
