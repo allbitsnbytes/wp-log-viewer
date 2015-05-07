2fb3fd117f6490fdb30e0043f2dce7ca2820ee9d
Maxwell Berkel <maxwell@allbitsnbytes.com>
2015-05-07 14:37:33 -0400

    Updated Log.php, added get_timezone, last_modified and clear

--- libs/Log.php
+++ libs/Log.php
@@ -20,9 +20,9 @@ use Allbitsnbytes\WPLogViewer\Helper;
  * @since 0.1.0
  */
 class Log {
-	
+
 	use IsSingleton;
-	
+
 	/**
 	 * Check if debug.log file exists
 	 * @return boolean True if debug.log exists
@@ -31,8 +31,8 @@ class Log {
 	public function file_exists() {
 		// TODO
 	}
-	
-	
+
+
 	/**
 	 * Check if debugging is enabled
 	 * @return boolean True if debugging is enabled
@@ -41,8 +41,8 @@ class Log {
 	public function debug_enabled() {
 		// TODO
 	}
-	
-	
+
+
 	/**
 	 * Check if file was modified since provided timestamp
 	 * @param $timestamp int Timestamp to check against
@@ -52,8 +52,28 @@ class Log {
 	public function is_modified($timestamp) {
 		// TODO
 	}
-	
-	
+
+
+	/**
+	 * Get last modified timestamp
+	 * @return int Last modified timestamp
+	 * @since 0.1.0
+	 */
+	public function last_modified() {
+		// TODO
+	}
+
+
+	/**
+	 * Get timezone for server
+	 * @return The server timezone
+	 * @since 0.1.0
+	 */
+	public function get_timezone() {
+		// TODO
+	}
+
+
 	/**
 	 * Check if file is smaller in size than provided size in bytes
 	 * @param $size int Size to check against
@@ -63,8 +83,8 @@ class Log {
 	public function is_smaller($size) {
 		// TODO
 	}
-	
-	
+
+
 	/**
 	 * Get all log entries
 	 * @return array Array of entries
@@ -73,8 +93,8 @@ class Log {
 	public function get_entries() {
 		// TODO
 	}
-	
-	
+
+
 	/**
 	 * Get all entries newer than specified timestamp
 	 * @param $timestamp int The oldest date and time to get entries for
@@ -82,6 +102,16 @@ class Log {
 	 * @since 0.1.0
 	 */
 	public function get_recent_entries($timestamp) {
+		// TODO
+	}
+
+
+	/**
+	 * Clear debug.log file
+	 * @return boolean True if file was cleared
+	 * @since 0.1.0
+	 */
+	public function clear() {
 		// TODO
 	}
 }
